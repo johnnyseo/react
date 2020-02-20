1172c863504b65c064eca480865e10377c012c5f
JohnnySeo <tjwhd6860@naver.com>
2020-02-20 17:55:45 +0900

    context-tutorial_HOC만드는 함수 만들기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import LeftPane from './components/LeftPane';
 import RightPane from './components/RightPane';
-import { SampleProvider } from './contexts/sample'
+import { SampleProvider } from './contexts/sample';
+import { AnotherProvider } from './contexts/another';
+import Counter from './components/Counter';
+
+const AppProvider = ({ contexts, children }) => contexts.reduce(
+  (prev, context) => React.createElement(context, {
+    children: prev
+  }), 
+  children
+);
 
 const App = () => {
   return (
-    <SampleProvider>
-       <div className="panes">
+    <AppProvider
+      contexts={[SampleProvider, AnotherProvider]}
+    >
+      <div className="panes">
         <LeftPane />
         <RightPane />
       </div>
-    </SampleProvider>   
+      <Counter />
+    </AppProvider>
   );
 };
 
