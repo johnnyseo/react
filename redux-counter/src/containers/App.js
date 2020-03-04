c3cd327fb7cde0eca448c7ae310046a295ac64c7
JohnnySeo <tjwhd6860@naver.com>
2020-03-04 15:32:40 +0900

    immutable 적용전

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import CounterContainer from './CounterContainer';
+import CounterContainer from './CounterListContainer';
+import { connect } from 'react-redux';
+import { getRandomColor } from '../utils';
+import * as actions from '../actions';
+import Buttons from '../components/Buttons';
 
 class App extends Component {
     render() {
+        const { onCreate, onRemove }  =this.props;
         return (
-            <div>
+            <div className="App">
+                <Buttons
+                    onCreate={onCreate}
+                    onRemove={onRemove} />
                 <CounterContainer />
             </div>
             
@@ -12,4 +20,11 @@ class App extends Component {
     }
 }
 
-export default App;
+// 액션함수
+const MapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: (index) => dispatch(actions.remove(index))
+})
+
+// 리덕스 연결시키고 내보냄
+export default connect(null, MapToDispatch)(App);
--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -1,38 +1,74 @@
 import * as types from '../actions/ActionTypes';
 
 const initialState = {
-    color: 'black',
-    number: 0
+    counters: [
+        {
+            color : 'black',
+            number: 0
+        }
+    ]    
 }
-
-/* 
-    리듀서 함수를 정의합니다. 리듀서는 state 와 action 을 파라미터로 받습니다.
-    state 가 undefined 일때 (스토어가 생성될때) state 의 기본값을 initialState 로 사용합니다.
-    action.type 에 따라 다른 작업을 하고, 새 상태를 만들어서 반환합니다.
-    이 때 주의 할 점은 state 를 직접 수정하면 안되고,
-    기존 상태 값에 원하는 값을 덮어쓴 새로운 객체를 만들어서 반환해야합니다.
-*/
 
 
 function counter(state = initialState, action){
-    switch (action.type) {
-        case types.INCREMENT:
+    // 레퍼런스 생성
+    const { counters } = state;
+
+    switch(action.type){
+        //카운터 생성
+        case types.CREATE : 
             return {
-                ...state,
-                number: state.number + 1
+                counters: [
+                    ...counters,
+                    {
+                        color: action.color,
+                        number: 0
+                    }
+                ]
             };
-        case types.DECREMENT:
+        // slice 를 이용하여 맨 마지막 카운터를 제외
+        case types.REMOVE : 
             return {
-                ...state,
-                number: state.number -1
+                counters: counters.slice(0, counters.length -1)
             };
-        case types.SET_COLOR:
+        // action.index 번째 카운터의 number 에 1 을 더합니다.
+        case types.INCREMENT : 
+            return  {
+                counters: [
+                    ...counters.slice(0, action.index),             // 0 ~ action.index 사이의 아이템들을 잘라와서 이 자리에 넣는다
+                    {
+                        ...counters[action.index],                  // 기존 값은 유지하면서
+                        number: counters[action.index].number + 1   // number 값을 덮어쓴다 
+                    },
+                    ...counters.slice(action.index + 1, counters.length)    // action.index + 1 ~ 마지막까지 잘라온
+                ]  
+            };
+        case types.DECREMENT : 
             return {
-                ...state,
-                color: action.color
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number - 1 
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
             };
-        default:
+        case types.SET_COLOR :
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+            
+        default : 
             return state;
+        
     }
 };
 
