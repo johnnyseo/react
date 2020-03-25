35be585b00f1d4f0860b83723174e724e4283361
JohnnySeo <tjwhd6860@naver.com>
2020-03-25 11:49:43 +0900

    first commit

---
+++ argon-react-native-master/constants/articles.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: '컨테이너 회전율 정보',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRzE_vjLYFhKWLeRA9DdYiC0xx9U6gi-pfOhnrn9A9aWJUHcPJ',
+    cta: 'View article', 
+    horizontal: true
+  },
+  {
+    title: '기능2',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'View article'
+  },
+  {
+    title: '기능3',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'View article' 
+  },
+  {
+    title: '기능4',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    cta: 'View article' 
+  },
+  {
+    title: '기능5',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    cta: 'View article', 
+    horizontal: true
+  },
+];
